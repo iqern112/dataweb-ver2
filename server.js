14ec7fb64e3408464aa77c17c37a0d1a7a24d532
butterpal <kralazit@gmail.com>
2024-10-30 17:12:54 +0700

    .

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ app.use(express.urlencoded({extended:false}));
 
 // การเชื่อมต่อฐานข้อมูล PostgreSQL
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        ssl: {
+            rejectUnauthorized: false
+        }
+
+        
 });
 
 // ฟังก์ชันสำหรับคิวรี่ข้อมูล nationality
