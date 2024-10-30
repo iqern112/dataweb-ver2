891349a6caf436389d2216b7f25d589f647f5a4b
iqern112 <sernsan89@gmail.com>
2024-10-30 16:27:19 +0700

    30/10/16.27

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // ฟังก์ชันสำหรับคิวรี่ข้อมูล nationality
